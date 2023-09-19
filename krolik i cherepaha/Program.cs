368e2f2e6365607a3c7c474fcf1793ab2212a533
Цомкалов Игорь <igorcomkalov@gmail.com>
2023-09-19 11:58:46 +0300

    Добавьте файлы проекта.

---
+++ krolik i cherepaha/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program1
+{
+    private static void Main(string[] args)
+    {
+        Thread cherepaha = new(MyThreadCherepaha);
+        cherepaha.Start();
+
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Кролик пробежал: " + i);
+        }
+    }
+    static void MyThreadCherepaha()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Черепаха пробежала: " + i);
+        }
+    }
+}
